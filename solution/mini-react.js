9184af4e763fbbf7b76d4f5c3106cea489433521
marcreativo <marcreativo@gmail.com>
2019-10-21 16:41:01 -0300

    wip

--- solution/mini-react.js
+++ solution/mini-react.js
@@ -1,74 +1,30 @@
 ;(() => {
-	let rootElement,
-		rootDomElement,
-		classCounter = 0
+	let rootElement, rootDomElement
+	let classCounter = 0
 	const classesGroup = {}
 
-	function isClass(fun) {
-		return typeof fun === 'function' && /^class\s/.test(Function.prototype.toString.call(func))
-	}
-
-	const isFunctionalComponent = (element) => {
-		return !isClass(element) && typeof element === 'function'
-	}
-
-	function shouldAddListener(prop) {
-		return /^on.*$/.test(prop)
-	}
-
-	const newElement = (element) => {
-		if (element.hasOwnProperty('tagName')) {
-			return handleHtmlElement(element)
-		} else if (element.hasOwnProperty('componentClass')) {
-			return handleClass(element)
-		} else if (isFunctionalComponent(element)) {
-			return element(props)
+	const appendProp = (element, name, value) => {
+		if (isAnEvent(name)) {
+			element.addEventListener(name.substring(2).toLowerCase(), value)
 		} else {
-			console.log(`no sé que es ${element.componentClass}`)
+			element.setAttribute(name, value)
 		}
 	}
 
-	const handleHtmlElement = ({ tagName, type, children, value, textContent, min, max, onchange }) => {
-		const domElement = document.createElement(tagName)
-		if (textContent) domElement.innerText = textContent
-		children && children.forEach((child) => appendChild(domElement, child))
-		//_.forEach(props, (value, name) => appendProp(domElement, name, value))
-		//Object.entries(props).forEach((prop) => appendProp(domElement, prop[0], prop[1]))
-		return domElement
-	}
-
-	const appendChild = (element, child) => {
-		if (child && child.componentClass) {
-			appendChild(element, child.render())
-		} else if (Array.isArray(child)) {
-			child.forEach((ch) => appendChild(element, ch))
-		} else if (typeof child === 'object') {
-			debugger
-			element.appendChild(child)
-		} else {
-			element.innerHTML += child
-		}
+	const isAnEvent = (property) => {
+		return /^on.*$/.test(property)
 	}
 
 	const handleClass = (element) => {
 		let { componentClass, props } = element
-
 		classCounter++
 		if (classesGroup[classCounter]) {
 			return classesGroup[classCounter]
 		}
-		const reactElement = new componentClass(props)
+		const reactElement = new componentClass(element)
 		reactElement.render()
 		classesGroup[classCounter] = reactElement
 		return reactElement
-	}
-
-	const appendProp = (element, propName, propVal) => {
-		if (shouldAddEventListener(propName)) {
-			element.addEventListener(propName.substring(2).toLowerCase(), propVal)
-		} else {
-			element.setAttribute(propName, propVal)
-		}
 	}
 
 	class Component {
@@ -77,7 +33,42 @@
 		}
 
 		setState(state) {
-			this.state = Object.assign({}, this.state, state)
+			let newState = state()
+			this.state = { ...this.state, ...newState }
+			reRender()
+		}
+	}
+
+	const reRender = () => {
+		classCounter = 0
+		rootDomElement.innerHTML = ''
+		MiniReact.render(rootElement, rootDomElement)
+	}
+
+	const appendChild = (element, child) => {
+		if (child.componentClass) {
+			appendChild(element, child.render())
+		} else if (child instanceof Array) {
+			child.forEach((children) => appendChild(element, children))
+		} else if (child instanceof HTMLElement) {
+			element.appendChild(child)
+		}
+	}
+
+	const handleHtmlElement = (props) => {
+		let { tagName, textContent, children } = props
+		const domElement = document.createElement(tagName)
+		if (textContent) domElement.innerText = textContent
+		children && children.forEach((child) => appendChild(domElement, child))
+		Object.entries(props).forEach((prop) => appendProp(domElement, prop[0], prop[1]))
+		return domElement
+	}
+
+	const newElement = (element) => {
+		if (element.tagName) {
+			return handleHtmlElement(element)
+		} else if (element.componentClass) {
+			return handleClass(element)
 		}
 	}
 
